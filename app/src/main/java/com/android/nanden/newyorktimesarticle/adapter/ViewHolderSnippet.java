877e14307a32dec4e13fdee50a4135c0c09b960a
Nan Den <nannandenden@gmail.com>
2017-09-25 15:25:44 -0700

    add some lambda expression

--- app/src/main/java/com/android/nanden/newyorktimesarticle/adapter/ViewHolderSnippet.java
+++ app/src/main/java/com/android/nanden/newyorktimesarticle/adapter/ViewHolderSnippet.java
@@ -24,16 +24,14 @@ public class ViewHolderSnippet extends RecyclerView.ViewHolder {
     public ViewHolderSnippet(final View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
-        itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ArticleAdapter.getOnItemClickListener() != null) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION) {
-                        ArticleAdapter.getOnItemClickListener().onItemClick(itemView, position);
-                    }
+        itemView.setOnClickListener(v -> {
+            if (ArticleAdapter.getOnItemClickListener() != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    ArticleAdapter.getOnItemClickListener().onItemClick(itemView, position);
                 }
             }
+
         });
     }
 
